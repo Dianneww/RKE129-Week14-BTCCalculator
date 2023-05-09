5126baa62aa7cbc6a30abae734733c136455ae50
Dianneww <diana.lagoida@tktk.ee>
2023-05-09 12:41:26 +0300

    add week 14 code

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const axios = require('axios');
+const exp = require('constants');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    res.render('index', {data: ''});
+})
+
+app.post('/calculate', (req, res) =>{
+    let userCoins = req.body.userCoins;
+    let userCurrency = req.body.currency;
+
+    let url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+
+    axios.get(url)
+    .then(response => {
+        let bitcoinData = response.data;
+        let result ={
+            code: '',
+            worth: '',
+            disclaimer: bitcoinData.disclaimer
+        }
+        if(userCurrency ==='EUR'){
+            result.code = bitcoinData.bpi.EUR.code;
+            result.worth = userCoins * bitcoinData.bpi.EUR.rate_float;
+        }
+        else if(userCurrency === 'USD'){
+            result.code = bitcoinData.bpi.USD.code;
+            result.worth = userCoins * bitcoinData.bpi.USD.rate_float;
+        }
+        res.render('index',{data: result});
+    })
+});
+
+let port = 5000;
+app.listen(port, () =>{
+    console.log(`Server is running ${port} now.`);
+})
